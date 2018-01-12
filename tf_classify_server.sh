ce989e01a0651c81b48bacdfbc3ebbf37d7f8ee1
Bowei Liu <liubowei@gmail.com>
2018-01-12 04:00:20 +0000

    fixup basic_proxy

--- tf_classify_server.sh
+++ tf_classify_server.sh
@@ -4,5 +4,6 @@ python tf_classify_server.py \
     --graph=/tmp/output_graph.pb \
     --labels=/tmp/output_labels.txt \
     --output_layer=final_result:0 \
-    --image=$HOME/flower_photos/daisy/21652746_cc379e0eea_m.jpg
+    --image=$HOME/flower_photos/daisy/21652746_cc379e0eea_m.jpg \
+    "$@"
 
