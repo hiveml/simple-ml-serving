23897022a4f0dade9a41d9ee697b25dc56053ba5
Bowei Liu <liubowei@gmail.com>
2018-01-12 03:54:24 +0000

    better basic_proxy

--- basic_proxy.js
+++ basic_proxy.js
@@ -1,13 +1,17 @@
 // Usage : node basic_proxy.js WORKER_PORT_0,WORKER_PORT_1,...
-var proxy = require('http-proxy').createProxyServer({})
-var worker_ports = process.argv[2].split(',')
+const worker_ports = process.argv[2].split(',')
 if (worker_ports.length === 0) { console.err('missing worker ports') ; process.exit(1) }
-var i = 0
+
+const proxy = require('http-proxy').createProxyServer({})
+proxy.on('error', () => console.log('proxy error'))
+
+let i = 0
 require('http').createServer((req, res) => {
-  var this_port = worker_ports[ (i++) % worker_ports.length ]
-  console.log(this_port)
-  proxy.web(req,res, {target: 'http://localhost:' + this_port})
+  proxy.web(req,res, {target: 'http://localhost:' + worker_ports[ (i++) % worker_ports.length ]})
 }).listen(12480)
 console.log(`Proxying localhost:${12480} to [${worker_ports.toString()}]`)
-proxy.on('error', () => console.log('proxy error'))
-  
+
+// spin up the ML workers
+const { exec } = require('child_process')
+worker_ports.map(port => exec(`/bin/bash ./tf_classify_server.sh ${port}`))
+
