9ef0c1e47655eae0fb35b8c8725c9ff9916c00db
Bowei Liu <liubowei@gmail.com>
2018-01-12 01:49:38 +0000

    self contained test tf classify

--- test_tf_classify_server.sh
+++ test_tf_classify_server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl -v -XPOST localhost:12480 -F"data=$HOME/flower_photos/daisy/21652746_cc379e0eea_m.jpg"
+bash tf_classify_server.sh &
+echo 'started server ; sleeping 5'
+sleep 5
+curl -v -XPOST localhost:12480 -F"data=@$HOME/flower_photos/daisy/21652746_cc379e0eea_m.jpg"
