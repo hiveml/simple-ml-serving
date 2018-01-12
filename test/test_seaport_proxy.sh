672fc6cde0bac396b3641aaa0d35caececc83910
Bowei Liu <liubowei@gmail.com>
2018-01-12 06:04:28 +0000

    rename things

--- test/test_seaport_proxy.sh
+++ test/test_seaport_proxy.sh
@@ -6,11 +6,12 @@ cd "$HOME"
 node seaport_proxy.js &
 node tf_classify_server.js &
 sleep 5
-curl -v -XPOST localhost:12480 -F"data=@$HOME/flower_photos/daisy/21652746_cc379e0eea_m.jpg"
+bash tv_classify_client.js
+
 node tf_classify_server.js &
 node tf_classify_server.js &
 sleep 5
-curl -v -XPOST localhost:12480 -F"data=@$HOME/flower_photos/daisy/21652746_cc379e0eea_m.jpg"
-curl -v -XPOST localhost:12480 -F"data=@$HOME/flower_photos/daisy/21652746_cc379e0eea_m.jpg"
-curl -v -XPOST localhost:12480 -F"data=@$HOME/flower_photos/daisy/21652746_cc379e0eea_m.jpg"
+bash tv_classify_client.js
+bash tv_classify_client.js
+bash tv_classify_client.js
 
