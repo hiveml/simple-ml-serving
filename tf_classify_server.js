72a57dcfe016fcfc826b825493c57e3022ce7095
Bowei Liu <liubowei@gmail.com>
2018-01-12 05:39:57 +0000

    simplify p2pproxy

--- tf_classify_server.js
+++ tf_classify_server.js
@@ -1,4 +1,4 @@
-// Usage : node seaport_worker.js
+// Usage : node tf_classify_server.js
 const port = require('seaport').connect(12481).register('tf_classify_server')
 console.log(`Launching tf classify worker on ${port}`)
 require('child_process').exec(`/bin/bash ./tf_classify_server.sh ${port}`)
